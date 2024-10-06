ef6779e8fc242530004ae1b7d45d681f3be0194d
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-06 21:54:21 +0530

    added discrod,twitch,figma,leetcode,spotify,appleMusic svg and updated the package to latest versions

--- src/components/BentoLayout.js
+++ src/components/BentoLayout.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { gsap } from 'gsap';
-import githubLogo from '../assets/github.svg';
-import linkedInLogo from '../assets/linkedin.svg';
-import xLogo from '../assets/x-logo.svg';
-import mailLogo from '../assets/mail.png';
-import experienceImage from '../assets/experience.svg';
-import resumeImage from '../assets/resume.svg';
-import backgroundImage from '../assets/profile.png';
-import setupImage from '../assets/setup.jpg';
-import { email, github, linkedin, twitter , resume} from '../profileconfig';
-
+import React, { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+import githubLogo from "../assets/github.svg";
+import linkedInLogo from "../assets/linkedin.svg";
+import xLogo from "../assets/x-logo.svg";
+import mailLogo from "../assets/mail.png";
+import experienceImage from "../assets/experience.svg";
+import resumeImage from "../assets/resume.svg";
+import backgroundImage from "../assets/profile.png";
+import setupImage from "../assets/setup.jpg";
+import { email, github, linkedin, twitter, resume } from "../profileconfig";
 
 const BentoLayout = () => {
   const gridRef = useRef(null);
@@ -17,7 +16,11 @@ const BentoLayout = () => {
 
   useEffect(() => {
     const elements = gridRef.current.children;
-    gsap.set(elements, { opacity: 0, y: () => -Math.random() * 500 - 100, x: () => 50 - Math.random() * 100 });
+    gsap.set(elements, {
+      opacity: 0,
+      y: () => -Math.random() * 500 - 100,
+      x: () => 50 - Math.random() * 100,
+    });
     gsap.to(elements, {
       opacity: 1,
       y: 0,
@@ -26,7 +29,7 @@ const BentoLayout = () => {
       ease: "elastic.out(1, 0.75)",
       duration: 1.2,
       delay: 0.5,
-      onComplete: () => setAnimationDone(true)
+      onComplete: () => setAnimationDone(true),
     });
   }, []);
 
@@ -34,66 +37,109 @@ const BentoLayout = () => {
     if (animationDone) {
       const elements = gridRef.current.children;
       for (let element of elements) {
-        element.addEventListener('mouseenter', () => {
-          gsap.to(element, { scale: 1.1, duration: 0.2, ease: "Power1.easeOut", overwrite: 'auto', zIndex: 10 });
+        element.addEventListener("mouseenter", () => {
+          gsap.to(element, {
+            scale: 1.1,
+            duration: 0.2,
+            ease: "Power1.easeOut",
+            overwrite: "auto",
+            zIndex: 10,
+          });
         });
-        element.addEventListener('mouseleave', () => {
-          gsap.to(element, { scale: 1, duration: 0.2, ease: "Power1.easeOut", overwrite: 'auto', zIndex: 1 });
+        element.addEventListener("mouseleave", () => {
+          gsap.to(element, {
+            scale: 1,
+            duration: 0.2,
+            ease: "Power1.easeOut",
+            overwrite: "auto",
+            zIndex: 1,
+          });
         });
       }
     }
   }, [animationDone]);
 
   return (
-    <div ref={ gridRef } className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+    <div
+      ref={gridRef}
+      className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4"
+    >
       <div
         className="col-span-1 sm:col-span-2 lg:col-span-2 aspect-square sm:aspect-[2/1] rounded-xl border-transparent p-4 shadow-md flex justify-center items-center"
-        style={ {
-          backgroundImage: `url(${ backgroundImage })`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center'
-        } }
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <span className="text-xl lg:text-4xl text-black font-bold">Who Am I?</span>
+        <span className="text-xl lg:text-4xl text-black font-bold">
+          Who Am I?
+        </span>
       </div>
 
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
         <a href={resume} target="_blank" rel="noopener noreferrer">
-          <img src={ resumeImage } alt="Resume" className="w-full h-full object-cover"/>
+          <img
+            src={resumeImage}
+            alt="Resume"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
 
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
-        <img src={ experienceImage } alt="Experience" className="w-full h-full object-cover"/>
+        <img
+          src={experienceImage}
+          alt="Experience"
+          className="w-full h-full object-cover"
+        />
       </div>
-      <div
-        className="col-span-1 sm:col-span-2 lg:col-span-4 aspect-square sm:aspect-[4/1] rounded-xl border-transparent shadow-md overflow-hidden"
-      >
-        <img src={ setupImage } alt="Setup" className="w-full h-full object-cover"/>
+      <div className="col-span-1 sm:col-span-2 lg:col-span-4 aspect-square sm:aspect-[4/1] rounded-xl border-transparent shadow-md overflow-hidden">
+        <img
+          src={setupImage}
+          alt="Setup"
+          className="w-full h-full object-cover"
+        />
       </div>
 
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
         <a href={github} target="_blank" rel="noopener noreferrer">
-          <img src={githubLogo} alt="GitHub Logo" className="w-full h-full object-cover"/>
+          <img
+            src={githubLogo}
+            alt="GitHub Logo"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
         <a href={linkedin} target="_blank" rel="noopener noreferrer">
-          <img src={ linkedInLogo } alt="LinkedIn Logo" className="w-full h-full object-cover"/>
+          <img
+            src={linkedInLogo}
+            alt="LinkedIn Logo"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
         <a href={twitter} target="_blank" rel="noopener noreferrer">
-          <img src={ xLogo } alt="XTwitter" className="w-full h-full object-cover"/>
+          <img
+            src={xLogo}
+            alt="XTwitter"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
       <div className="col-span-1 aspect-square rounded-xl border-transparent shadow-md overflow-hidden">
         <a href={`mailto:${email}`}>
-          <img src={ mailLogo } alt="Mail Logo" className="w-full h-full object-cover"/>
+          <img
+            src={mailLogo}
+            alt="Mail Logo"
+            className="w-full h-full object-cover"
+          />
         </a>
       </div>
     </div>
   );
 };
 
-export default BentoLayout
+export default BentoLayout;
